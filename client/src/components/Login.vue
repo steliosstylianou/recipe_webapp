9d91f7e701bdfea969890f660110b04310df35ed
Unknown <ec15441@my.bristol.ac.uk>
2018-05-10 15:38:48 +0100

    Login page added

---
+++ client/src/components/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <body>
+  <section class="hero is-success is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-4 is-offset-4">
+          <h3 class="title has-text-grey">Login</h3>
+          <p class="subtitle has-text-grey">Please enter your details to log in</p>
+          <div class="box">
+            <form>
+              <b-field>
+                <b-input type="email"
+                         v-model="email"
+                         placeholder="Email Address"
+                         rounded>
+                </b-input>
+              </b-field>
+
+              <b-field>
+                <b-input type="password"
+                         v-model="password"
+                         placeholder="Password"
+                         rounded
+                         password-reveal>
+                </b-input>
+              </b-field>
+
+              <button @click="register" class="button is-block is-info is-medium is-fullwidth is-rounded">Login</button>
+              <br/>
+
+            </form>
+          </div>
+          <p class="has-text-grey">
+            <a href=".">Forgot Password</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  </body>
+</template>
+
+<script>
+  import particlesJS from './ParticlesJS'
+  import Authentication from '@/services/Authentication'
+
+  export default {
+    components: {particlesJS},
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+
+    methods:{
+
+      async login (){
+        const response = await Authentication.login({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data);
+      }
+
+    }
+  }
+</script>
+
+<style>
+  .hero.is-success {
+    background: #F2F6FA;
+  }
+
+  .hero .nav, .hero.is-success .nav {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+
+  .box {
+    margin-top: 2rem;
+  }
+
+  input {
+    font-weight: 300;
+  }
+
+  p {
+    font-weight: 700;
+  }
+
+  p.subtitle {
+    padding-top: 2rem;
+  }
+
+</style>
+
+
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -16,6 +16,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
