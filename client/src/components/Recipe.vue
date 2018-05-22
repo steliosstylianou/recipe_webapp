daae10f4e25a73a092302d13834f801fc5db4fde
Eleni Stylianou <styl.eleni@gmail.com>
2018-05-22 14:14:59 +0100

    recipe page initial

--- client/src/components/Recipe.vue
+++ client/src/components/Recipe.vue
@@ -1,10 +1,29 @@
 <template>
     <section class="section">
-      <div class="container">
-        <h1 class="title is-left">Section</h1>
-        <h2 class="subtitle">
-          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
-        </h2>
-      </div>
+        <h1 class="title">Recipe Name</h1>
+        <h2 class="subtitle">Author Name</h2>
+      <article class="media">
+        <figure class="media-left">
+          <p class="image">
+            <img src="../assets/cookies.jpg">
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>Ingredients</strong>
+              <br>
+              Ingredients List
+            </p>
+          </div>
+          <div class="content">
+            <p>
+              <strong>Method</strong>
+              <br>
+              Method Description
+            </p>
+          </div>
+        </div>
+      </article>
     </section>
 </template>
