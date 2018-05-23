daa962243582c1a649d2b2b91f7096adf14435e1
Eleni Stylianou <styl.eleni@gmail.com>
2018-05-23 22:52:46 +0100

    layout fixed

--- client/src/components/Recipe.vue
+++ client/src/components/Recipe.vue
@@ -2,45 +2,59 @@
     <section class="section">
         <h1 class="title">Recipe Name</h1>
         <h2 class="subtitle">Author Name</h2>
-      <article class="media">
-        <figure class="media-left">
-          <p class="image">
-            <img src="../assets/cookies.jpg">
-          </p>
-        </figure>
-        <div class="media-content">
-          <div class="columns is-multiline">
-            <div class="column">
-              <strong>Difficulty</strong>
+      <div class="columns is-multiline">
+        <div class="column">
+          <strong>Difficulty</strong>
+          <br>
+          Beginner
+        </div>
+        <div class="column">
+          <strong>Total Time</strong>
+          <br>
+          35'
+        </div>
+        <div class="column">
+          <strong>Rating</strong>
+          <br>
+          5/5
+        </div>
+        <div class="column">
+          <button class="button is-medium is-rounded">
+            Favourite
+          </button>
+        </div>
+      </div>
+      <div class="columns is-mobile">
+        <div class="column is-three-fifths">
+          <figure class="media-left">
+            <p class="image">
+              <img src="../assets/cookies.jpg">
+            </p>
+          </figure>
+          <br>
+          <div class="content-">
+            <p>
+              <strong>Method</strong>
               <br>
-              Beginner
-            </div>
-            <div class="column">
-              <strong>Total Time</strong>
-              <br>
-              35'
-            </div>
-            <div class="column">
-              <strong>Rating</strong>
-              <br>
-              5/5
-            </div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget urna enim. Ut cursus feugiat mattis. Donec vitae gravida augue. Aenean scelerisque metus ac erat accumsan, quis iaculis tellus pretium. Suspendisse ultrices semper odio, quis tristique lorem vulputate at. Quisque luctus nulla vel rhoncus lobortis. Vestibulum euismod ut ipsum in faucibus. Aenean fermentum vel ligula sed pulvinar. Suspendisse potenti. Donec et velit mattis, bibendum dui et, fermentum turpis.
+
+              Maecenas vulputate neque eu imperdiet iaculis. Nunc in egestas mauris. Phasellus tellus lacus, volutpat sit amet eleifend sit amet, tempor a eros. Praesent efficitur in nunc eget interdum. Maecenas vitae interdum quam. Aenean sodales ac magna sed finibus. Aliquam pharetra placerat quam, a finibus sem fermentum sed. Praesent ac mi fermentum, fermentum ante vel, condimentum purus. Nunc vitae molestie orci. Cras gravida nulla semper, eleifend massa in, gravida est. Nam rhoncus elit vel nisi aliquet vestibulum. Cras pellentesque, sapien id molestie fermentum, eros felis facilisis lectus, nec pharetra orci magna a ligula. Maecenas volutpat porta eros, ut consequat libero posuere porta. Nam quis tortor ac justo egestas blandit. Pellentesque quis odio eu dolor dignissim hendrerit fringilla nec ipsum. Duis facilisis commodo ligula, id dignissim metus fringilla a.
+
+              Proin sit amet felis ac massa dapibus ornare id nec magna. Aenean erat neque, consequat in tellus a, molestie facilisis orci. Aliquam vel tincidunt arcu. Cras felis dui, pretium id gravida nec, sollicitudin quis nibh. Vivamus bibendum justo nec orci pulvinar, eget feugiat metus pretium. Donec vehicula varius faucibus. Aliquam at interdum neque, ac vulputate tellus.
+            </p>
           </div>
+        </div>
+        <div class="column">
           <div class="content">
             <p>
               <strong>Ingredients</strong>
               <br>
-              Ingredients List
-            </p>
-          </div>
-          <div class="content">
-            <p>
-              <strong>Method</strong>
-              <br>
-              Method Description
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget urna enim. Ut cursus feugiat mattis. Donec vitae gravida augue. Aenean scelerisque metus ac erat accumsan, quis iaculis tellus pretium. Suspendisse ultrices semper odio, quis tristique lorem vulputate at. Quisque luctus nulla vel rhoncus lobortis. Vestibulum euismod ut ipsum in faucibus. Aenean fermentum vel ligula sed pulvinar. Suspendisse potenti. Donec et velit mattis, bibendum dui et, fermentum turpis.
+
+              Maecenas vulputate neque eu imperdiet iaculis. Nunc in egestas mauris. Phasellus tellus lacus, volutpat sit amet eleifend sit amet, tempor a eros. Praesent efficitur in nunc eget interdum. Maecenas vitae interdum quam. Aenean sodales ac magna sed finibus. Aliquam pharetra placerat quam, a finibus sem fermentum sed. Praesent ac mi fermentum, fermentum ante vel, condimentum purus. Nunc vitae molestie orci. Cras gravida nulla semper, eleifend massa in, gravida est. Nam rhoncus elit vel nisi aliquet vestibulum. Cras pellentesque, sapien id molestie fermentum, eros felis facilisis lectus, nec pharetra orci magna a ligula. Maecenas volutpat porta eros, ut consequat libero posuere porta. Nam quis tortor ac justo egestas blandit. Pellentesque quis odio eu dolor dignissim hendrerit fringilla nec ipsum.
             </p>
           </div>
         </div>
-      </article>
+      </div>
     </section>
 </template>
