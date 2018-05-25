6578c8badf0c4cb5813423bcfd439da8ae1a9ae7
Eleni Stylianou <styl.eleni@gmail.com>
2018-05-25 21:25:57 +0100

    trying to make nav responsive

--- client/src/components/Navigator.vue
+++ client/src/components/Navigator.vue
@@ -5,7 +5,12 @@
         <div class="container is-fluid">
           <div class="navbar-brand">
             <img id="nav-logo" src="../assets/logo_chef.svg" @click="navigateTo({name: 'Login'})">
-            <span class="navbar-burger burger" data-target="navbarMenuHeroA"></span>
+            <!--<span class="navbar-burger burger" data-target="navbarMenuHeroA"></span>-->
+            <a role="button" class="navbar-burger" data-target="navbarMenuHeroA" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
           <div id="navbarMenuHeroA" class="navbar-menu">
             <div class="navbar-start">
@@ -88,6 +93,24 @@
 </template>
 
 <script>
+  document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+          // Get the target from the "data-target" attribute
+          var target = $el.dataset.target
+          var $target = document.getElementById(target)
+          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+          $el.classList.toggle('is-active')
+          $target.classList.toggle('is-active')
+        })
+      })
+    }
+  })
   export default {
     methods: {
       navigateTo (route) {
