f0b169b88b72d04c97c12f50b24b7c213441a848
Unknown <stylianoustelioscy@gmail.com>
2018-05-25 06:12:59 +0100

    validation for recipes dont but doesnt seem to be catching exceptions

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,31 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const config = require('./config/config');
-// const multer = require('multer');
+const multer = require('multer');
+// const RecipesController = require('./controllers/RecipesController');
+
+const upload = multer(config.multer).single('recipe');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
+
+app.use('/uploads', express.static('uploads'));
 
 require('./routes')(app);
 
 app.listen(config.port);
 
 console.log('Server started on port ' + config.port);
+
+app.post('/recipes', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.send({error: 'invalid_file'});
+        }
+        console.log('Ok');
+    });
+    console.log(req.body); // form fields
+});
